69c07b24c3f64121c6421a7f8c77f3ce347489f1
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-10 19:32:47 +0900

    test v4 function

--- functions/src/index.js
+++ functions/src/index.js
@@ -5,14 +5,11 @@ app.http('httpTrigger1', {
   authLevel: 'anonymous',
   route: 'line/callback',
   handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = (request.query.name || (request.body && request.body.name));
-    const responseMessage = name
-      ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-      : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const name = request.query.get('name') || (await request.text()) || 'world';
 
-
-    return { body: responseMessage };
+    return { body: `Hello, ${name}!` };
   },
 });
 
@@ -22,13 +19,10 @@ app.http('httpTrigger1', {
   authLevel: 'anonymous',
   route: 'health',
   handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = (request.query.name || (request.body && request.body.name));
-    const responseMessage = name
-      ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-      : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const name = request.query.get('name') || (await request.text()) || 'world';
 
-
-    return { body: responseMessage };
+    return { body: `Hello, ${name}!` };
   },
 });
