fcccccaabffacd1bd40f4152f0d253a729e97a4a
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-11 10:19:13 +0900

    callback function get line token

--- functions/src/functions/callback.js
+++ functions/src/functions/callback.js
@@ -1,14 +1,46 @@
 const { app } = require('@azure/functions');
 
 app.http('client', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    route: 'line/callback',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  route: 'line/callback',
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+    const code = req.query.get('code')
 
-        return { body: `Hello, ${name}!` };
+    if (!code) {
+      return { status: 400, body: 'Missing code' };
     }
+
+    try {
+      const data = new URLSearchParams();
+      data.append('grant_type', 'authorization_code');
+      data.append('code', code);
+      data.append('redirect_uri', `${process.env.FUNCTIONS_URL}/api/line/callback`);
+      data.append('client_id', process.env.LINE_CHANNEL_ID);
+      data.append('client_secret', process.env.LINE_CHANNEL_SECRET);
+
+      const tokenResponse = await fetch('https://api.line.me/oauth2/v2.1/token', {
+        method: 'POST',
+        body: data
+      }).then(res => res.json());
+
+      // const accessToken = tokenResponse.data.access_token;
+
+      // const profileResponse = await fetch('https://api.line.me/v2/profile', {
+      //   headers: { Authorization: `Bearer ${accessToken}` }
+      // }).then(res => res.json());
+
+      return {
+        body: tokenResponse
+      }
+
+    } catch (error) {
+      return {
+        status: 500,
+        body: error.message
+      }
+    }
+  }
 });
